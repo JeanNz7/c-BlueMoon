d0a0fef865b4a8970c657d8f5f2dfa4f4ba2c345
JeaNz7 <jeanmichel.jm33@gmail.com>
2023-12-05 16:05:35 -0300

    Adicionar index.html, script.js e styles.css

---
+++ script.js
@@ -0,0 +1,50 @@
+// script.js
+
+var estoque = JSON.parse(localStorage.getItem('estoque')) || [];
+var historicoLogin = JSON.parse(localStorage.getItem('historicoLogin')) || [];
+var isLoggedIn = false;
+var currentUser = '';
+
+function login() {
+    // ... (código JavaScript da função login)
+}
+
+function logout() {
+    // ... (código JavaScript da função logout)
+}
+
+function showPage(page) {
+    // ... (código JavaScript da função showPage)
+}
+
+function adicionarProduto() {
+    // ... (código JavaScript da função adicionarProduto)
+}
+
+function removerProduto() {
+    // ... (código JavaScript da função removerProduto)
+}
+
+function atualizarEstoque() {
+    // ... (código JavaScript da função atualizarEstoque)
+}
+
+function limparEstoque() {
+    // ... (código JavaScript da função limparEstoque)
+}
+
+function registrarHistoricoLogin(acao, produto, quantidade) {
+    // ... (código JavaScript da função registrarHistoricoLogin)
+}
+
+function limparHistorico() {
+    // ... (código JavaScript da função limparHistorico)
+}
+
+function salvarEstoqueNoLocalStorage() {
+    // ... (código JavaScript da função salvarEstoqueNoLocalStorage)
+}
+
+function salvarHistoricoLoginNoLocalStorage() {
+    // ... (código JavaScript da função salvarHistoricoLoginNoLocalStorage)
+}
